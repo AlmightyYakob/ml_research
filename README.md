b357dc03c189cda441a65395ef0a6f5c1d59b2d3
Jacob Nesbitt <jjnesbitt2@gmail.com>
2019-09-30 17:47:04 -0400

    Update deps, README

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # ml_research
 
+## Requirements
+* [POMDP Gym Extension](https://github.com/d3sm0/gym_pomdp)
+* Tensorflow
 
-# Resources
+## Resources
 * https://arxiv.org/pdf/1609.05521.pdf
--- battleship.py
+++ battleship.py
@@ -1,22 +1,40 @@
-import time
-import gym
 import gym_pomdp  # NOQA
 
-env = gym.make("Battleship-v0")
-env.reset()
+import time
+import gym
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, Flatten, Activation
+from keras.callbacks import ModelCheckpoint
 
-print("Action Space:", env.action_space)
-print("Obs Space:", env.observation_space)
 
-for i in range(env.action_space.n):
-    time.sleep(0.5)
+def model():
+    model = Sequential()
+    model.add(LSTM(64, input_shape=()))
+    model.add(Activation("relu"))
 
-    # observation, reward, done, info = env.step(env.action_space.sample())
-    observation, _, done, info = env.step(i)
-    print(observation)
-    env.render()
+    return model
 
-    if done:
-        break
 
-env.close()
+def main():
+    env = gym.make("Battleship-v0")
+    env.reset()
+
+    print("Action Space:", env.action_space)
+    print("Obs Space:", env.observation_space)
+
+    for i in range(env.action_space.n):
+        time.sleep(0.5)
+
+        # observation, reward, done, info = env.step(env.action_space.sample())
+        observation, _, done, info = env.step(i)
+        print(observation)
+        env.render()
+
+        if done:
+            break
+
+    env.close()
+
+
+if __name__ == "__main__":
+    main()
